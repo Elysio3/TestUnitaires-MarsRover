fde8028201cf91c1c179ffcbe5c7a68134c6d8f9
Elysio3 <mael.kerviche@hotmail.fr>
2023-03-01 14:18:30 +0100

    init Test file

---
+++ src/test/java/MarsRoverTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MarsRoverTest {
+
+    private MarsRover marsRover;
+
+    @BeforeEach
+    public void setUpEach() {
+        marsRover = new MarsRover();
+    }
+
+    @Test
+    public void shouldInitRover() {
+        // TODO Rover should be initialized to 0, 0, N
+        assertThat(marsRover.getPosition()).isEqualsTo("X : 0, Y : 0, Facing : N");
+    }
+
+    @Test
+    public void shouldMove() {
+        // TODO Rover should change position
+        marsRover.move(1);
+        assertThat(marsRover.getPosition()).isEqualsTo("X : 0, Y : 1, Facing : N");
+    }
+
+    @Test
+    public void shouldRotate() {
+        // TODO Rover should change facingDirection
+        marsRover.rotate('O');
+        assertThat(marsRover.getFacingDirection()).isEqualsTo('O');
+    }
+}
